809d637feed23ec4e7a250122ad57499dd9c5cc1
anirudh.chawla <anirudh.chawla@radixweb.com>
2022-06-28 17:00:57 +0530

    display skippings product

--- myskippingsite/settings.py
+++ myskippingsite/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 django_heroku.settings(locals())
